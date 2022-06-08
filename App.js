a16301f4579c5421303c1e05ac9fb0b8a456a2f8
theoofman <tyler@elsoutlet.com>
2022-06-08 11:27:08 -0700

    hello world

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("App executed");
+
+  let x = "f";
+  console.log(x.toString());
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
